44114da3c1956e6841061ed6b3cfb4d5f117f0d6
Akylbek Kerimbekov <akyl.kerimbekov@gmail.com>
2015-02-22 19:09:07 +0600

    add comments version 0.04

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## maybe my comments has mistakes, sorry for my bad english
 
-## Write a short comment describing this function
 
+## makeCacheMatrix consists of 4 functions and returns list of
+## functions. The main idea of this function is to store a matrix
+## and a cached inverse of that matrix.
 makeCacheMatrix <- function(x = matrix()) {
 
+	## this value stores a cached value of inversed matrix.
+	## at first initiate it has NULL value
 	cachedValue <- NULL
 
+	## setMatrix function reinitiate stored matrix and sets cached value to NULL
 	setMatrix <- function(newMatrix) {
 		x <<- newMatrix
 		cachedValue <<- NULL
 	}
 
+	## returns stored matrix
 	getMatrix <- function() {
 		return(x)
 	}
 
+	## stores cached value with given argument
 	cacheInverse <- function(solvedValue) {
 		cachedValue <<- solvedValue
 	}
 
+	## returns cached value
 	getInverse <- function() {
 		return(cachedValue)
 	}
 
+	## list with named elements, each element named by functions
 	list(setMatrix = setMatrix, getMatrix = getMatrix, 
 			cacheInverse = cacheInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve realizes the inversion of matrix, created by function makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## get the cached value of inversed matrix
         cachedValue <- x$getInverse()
+        ## if there is a cached value returns this value with message
         if (!is.null(cachedValue)) {
         	message("getting cached data")
         	return(cachedValue)
         }
+        ## else get matrix, calculate the inversion and store to cache
         newMatrix <- x$getMatrix()
         cachedValue <- solve(newMatrix)
         x$cacheInverse(cachedValue)
